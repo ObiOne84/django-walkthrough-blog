36b86cd23fd7d7ae1d93e4ae1ccf38121de8b985
ObiOne84 <stepienszymon@hotmail.com>
2023-11-09 18:25:09 +0000

    Deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = [
     '8000-obione84-djangowalkthro-qyxpz5n8x52.ws-eu106.gitpod.io',
